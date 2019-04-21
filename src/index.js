1b9a3470e17ee4132ef7ab9cf7d7b2065bd6f544
guakun01 <guakun01@gmail.com>
2019-04-21 18:17:13 +0800

    使用 umd 模式打包库

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import GuaButton from './gua-icon'
+//
+// export {
+//   GuaButton
+// }
+
+export { default as GuaButton } from './gua-button'
+export { default as GuaButtonGroup } from './gua-button-group'
+export { default as GuaIcon } from './gua-icon'
