7d4a1c3f36d504c7942efa4cf44dfcb08bf83327
guakun01 <guakun01@gmail.com>
2019-04-21 20:29:50 +0800

    暂存我的文档

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <gua-button>默认按钮</gua-button>
+  </div>
+</template>
+
+<script>
+import GuaButton from '../../../src/gua-button'
+
+export default {
+  components: {
+    GuaButton
+  }
+}
+</script>
---
+++ docs/components/button.md
@@ -0,0 +1,9 @@
+---
+title: 按钮
+---
+
+# 按钮组件
+
+使用方法
+
+<button-demos></button-demos>
