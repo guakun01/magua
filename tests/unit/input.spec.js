373babb4a342b724e978b9d4ee3a6df48d94bcf2
guakun01 <guakun01@gmail.com>
2019-05-07 16:49:24 +0800

    合并 input 事件的测试用例 去除重复

--- tests/unit/input.spec.js
+++ tests/unit/input.spec.js
@@ -52,41 +52,16 @@ describe('g-input.vue', () => {
     })
   })
   describe('event', () => {
-    it('支持 change 事件', () => {
-      const vm = mount(GInput, {}).vm
-      const callback = sinon.fake()
-      vm.$on('change', callback)
-      let inputElement = vm.$el.querySelector('input')
-      let event = new Event('change')
-      inputElement.dispatchEvent(event)
-      expect(callback).to.have.been.calledWith(event)
-    })
-    it('支持 input 事件', () => {
-      const vm = mount(GInput, {}).vm
-      const callback = sinon.fake()
-      vm.$on('input', callback)
-      let inputElement = vm.$el.querySelector('input')
-      let event = new Event('input')
-      inputElement.dispatchEvent(event)
-      expect(callback).to.have.been.calledWith(event)
-    })
-    it('支持 focus 事件', () => {
-      const vm = mount(GInput, {}).vm
-      const callback = sinon.fake()
-      vm.$on('focus', callback)
-      let inputElement = vm.$el.querySelector('input')
-      let event = new Event('focus')
-      inputElement.dispatchEvent(event)
-      expect(callback).to.have.been.calledWith(event)
-    })
-    it('支持 blur 事件', () => {
-      const vm = mount(GInput, {}).vm
-      const callback = sinon.fake()
-      vm.$on('blur', callback)
-      let inputElement = vm.$el.querySelector('input')
-      let event = new Event('blur')
-      inputElement.dispatchEvent(event)
-      expect(callback).to.have.been.calledWith(event)
+    it('支持 change/input/focus/blur 事件', () => {
+      ['change', 'input', 'focus', 'blur'].forEach(eventName => {
+        const vm = mount(GInput, {}).vm
+        const callback = sinon.fake()
+        vm.$on(eventName, callback)
+        let inputElement = vm.$el.querySelector('input')
+        let event = new Event(eventName)
+        inputElement.dispatchEvent(event)
+        expect(callback).to.have.been.calledWith(event)
+      })
     })
   })
 
