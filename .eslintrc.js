f38d8629027cfba64fc0972fcbe96b95c482cc2c
guakun01 <guakun01@gmail.com>
2019-05-09 16:19:54 +0800

    更新项目框架 移除 vuepress 兼容 ie

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': 'off'
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <gua-button icon="settings">
+      guagualoveu
+    </gua-button>
+    <gua-input></gua-input>
+  </div>
+</template>
+
+<script>
+import GuaButton from './button'
+import GuaInput from './input'
+
+export default {
+  components: {
+    GuaButton,
+    GuaInput
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
